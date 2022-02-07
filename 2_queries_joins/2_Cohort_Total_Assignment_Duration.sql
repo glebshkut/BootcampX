8aef33f598e61e3543b905b021b8221a2f16b90a
Gleb Shkut <gleb.shkut@gmail.com>
2022-02-07 15:48:17 -0800

    calculate time spent on assignment

---
+++ 2_queries_joins/2_Cohort_Total_Assignment_Duration.sql
@@ -0,0 +1,5 @@
+SELECT SUM(duration) AS total_duration
+FROM students
+JOIN assignment_submissions ON students.id = assignment_submissions.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = 'FEB12';
